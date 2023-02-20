cb5063f8b515abd157fc68dab199399ab754ec13
killyan35 <kilyan.gerard@3wa.io>
2023-02-20 12:49:52 +0100

    debut js

--- wp-content/themes/yep/front-page.php
+++ wp-content/themes/yep/front-page.php
@@ -1,3 +1,9 @@
+<?php  
+    $menuItems = getNavigationMenu(); 
+    $data = getHomepageData();  
+  
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -10,6 +16,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>  
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />  
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/index.js" /> 
 <title><?php echo get_bloginfo( 'name' );?></title>
     </head>
     
@@ -17,50 +24,16 @@
         <header>
             <nav>
                 <ul>
-                    <li><figure><?xml version="1.0" encoding="utf-8"?>
-                        <!-- Generator: Adobe Illustrator 27.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-                        <svg version="1.1" id="Calque_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                             viewBox="0 0 2250 1500" style="enable-background:new 0 0 2250 1500;" xml:space="preserve">
-                        <style type="text/css">
-                            .st0{fill:none;stroke:#FFFFFF;stroke-width:8;stroke-miterlimit:10;}
-                            .st1{fill:none;stroke:#EFEC4F;stroke-width:10;stroke-miterlimit:10;}
-                            .st2{fill:#EFEC4F;}
-                            .st3{fill:none;stroke:#4CEE7E;stroke-width:10;stroke-miterlimit:10;}
-                            .st4{fill:#4CEE7E;}
-                            .st5{fill:none;stroke:#427CF5;stroke-width:10;stroke-miterlimit:10;}
-                            .st6{fill:#427CF5;}
-                            .st7{fill:none;stroke:#956CAF;stroke-width:10;stroke-miterlimit:10;}
-                            .st8{fill:#956CAF;}
-                            .st9{fill:none;stroke:#ECA45C;stroke-width:10;stroke-miterlimit:10;}
-                            .st10{fill:#ECA45C;}
-                            .st11{fill:none;stroke:#E85B69;stroke-width:10;stroke-miterlimit:10;}
-                            .st12{fill:#E85B69;}
-                        </style>
-                        <line class="st0" x1="432.6" y1="181.1" x2="130.8" y2="679"/>
-                        <line class="st0" x1="609.6" y1="1116.9" x2="130.8" y2="679"/>
-                        <line class="st0" x1="1334.3" y1="984.1" x2="609.6" y2="1116.9"/>
-                        <line class="st0" x1="2097.2" y1="946.8" x2="1334.3" y2="984.1"/>
-                        <line class="st0" x1="1334.3" y1="278.9" x2="2097.2" y2="946.8"/>
-                        <line class="st0" x1="432.6" y1="181.1" x2="1334.3" y2="278.9"/>
-                        <line class="st0" x1="130.8" y1="679" x2="1334.3" y2="984.1"/>
-                        <line class="st0" x1="609.6" y1="1116.9" x2="432.6" y2="181.1"/>
-                        <line class="st0" x1="130.8" y1="679" x2="1343.1" y2="286.7"/>
-                        <line class="st0" x1="2097.2" y1="946.8" x2="435" y2="194"/>
-                        <line class="st0" x1="1334.3" y1="984.1" x2="1343.1" y2="286.7"/>
-                        <line class="st0" x1="609.6" y1="1116.9" x2="1343.1" y2="286.7"/>
-                        <circle class="st1" cx="432.6" cy="181.1" r="105.6"/>
-                        <circle class="st2" cx="432.6" cy="181.1" r="71.5"/>
-                        <circle class="st3" cx="130.8" cy="679" r="105.6"/>
-                        <circle class="st4" cx="130.8" cy="679" r="71.5"/>
-                        <circle class="st5" cx="609.6" cy="1116.9" r="105.6"/>
-                        <circle class="st6" cx="609.6" cy="1116.9" r="71.5"/>
-                        <circle class="st7" cx="1334.3" cy="984.1" r="105.6"/>
-                        <circle class="st8" cx="1334.3" cy="984.1" r="71.5"/>
-                        <circle class="st9" cx="1334.3" cy="278.9" r="105.6"/>
-                        <circle class="st10" cx="1334.3" cy="278.9" r="71.5"/>
-                        <circle class="st11" cx="2097.2" cy="946.8" r="105.6"/>
-                        <circle class="st12" cx="2097.2" cy="946.8" r="71.5"/>
-                        </svg></figure></li>
+                <?php foreach($menuItems as $item) { ?>  
+                    <li>  
+                        <a href="<?= $item->url ?>">  
+                            <?= $item->title ?>  
+                        </a>  
+                    </li>  
+                <?php  
+                }  
+                ?>
+                    <li id="fig"><figure><img src="wp-content/themes/yep/assets/img/Gaellan-Logo.svg"></figure></li>
                     <li>ACCUEIL</li>
                     <li>À PROPOS</li>
                     <li>MES PROJETS</li>
@@ -74,7 +47,7 @@
                     <h1>YEPFOLIO</h1>
                     <h2>DÉVELOPPEMENT WEB & MOBILE</h2>
                 </li>
-                <li>
+                <!-- <li>
                     <h3>YEPFOLIO EN QUELQUES MOTS</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
@@ -87,30 +60,35 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
+                </li> -->
+                <li>
+                    <section id="about">  
+                        <h2 id="titre"><?= $data["a-propos"]["titre"] ?></h2>  
+                        <p>  
+                            <?= $data["a-propos"]["contenu"] ?>  
+                            
+                        </p>  
+                    </section>
                 </li>
+                <?php 
+                echo "<pre>";
+                var_dump($data["projets"]); 
+                echo "</pre>";
+                var_dump($data); 
+                ?>
+                <section id="posts">  
+                <?= $data["projets"][2]["category"] ?>
+            </section>
                 <li>
                     <ul>
                         <h2>MES DERNIERS PROJETS</h2>
-                            <li>
-                                <h3>APPLICATION SANTÉ</h3>
-                                <h4>Pharmacie de Maurepas</h4>
+                        <?php
+                        foreach($data["projets"] as $projets){ ?>
+                        <li  >
+                                <h3><?= $projets["category"]; ?> </h3>
+                                <h4><?= $projets["data"]->post_title; ?></h4>
                             </li>
-                            <li>
-                                <h3>SITE E-COMMERCE</h3>
-                                <h4>Librairie l'écume des jours</h4>
-                            </li>
-                            <li>
-                                <h3>SITE VITRINE</h3>
-                                <h4>Boulangerie Gaitier</h4>
-                            </li>
-                            <li>
-                                <h3>SITE INSTITUTIONNEL</h3>
-                                <h4>Mairie de Ploutruc</h4>
-                            </li>
-                            <li>
-                                <h3>SITE PROMOTIONNEL</h3>
-                                <h4>Festival des choses</h4>
-                            </li>
+                    <?php    } ?>
                     </ul>
                 </li>
             </ul>
